f9bc28a7e8426a4a139c47199f8bd45b3d1fbd84
seomy <muyear.seo@gmail.com>
2022-11-01 19:17:20 +0900

    security setting

--- src/main/java/com/seo/security1/controller/IndexController.java
+++ src/main/java/com/seo/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.seo.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -13,5 +14,34 @@ public class IndexController {
 		// viewResolve setting : templates(prefix), .mustache (suffix) 
 		return "index"; // src/main/resources/templates/index.mustache
 	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
 
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "会員登録完了しました！";
+	}
 }
