1ed8ac28916e60431cc207d1a6f4b6228668d47d
seomy <muyear.seo@gmail.com>
2022-11-02 15:04:38 +0900

    join

---
+++ src/main/java/com/seo/security1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.seo.security1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.seo.security1.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+}
